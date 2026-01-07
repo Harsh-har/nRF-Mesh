767f301698ec0ea80f662c0cad78311a685be486
Harsh-har <singhalharsh379@gmail.com>
2026-01-07 19:06:59 +0530

    Remove Generic on off ui and logic and add Command and parameter ui and backend

--- app/src/main/java/no/nordicsemi/android/nrfmesh/MainActivity.java
+++ app/src/main/java/no/nordicsemi/android/nrfmesh/MainActivity.java
@@ -9,7 +9,6 @@ import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.splashscreen.SplashScreen;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
